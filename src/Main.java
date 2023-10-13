50a8585ecd8b2713ba7bcc058749efb7f6ae1bd0
jo sung moo <wh9555115@naver.com>
2023-10-13 15:48:04 +0900

    하드모드 구현, 시간제한 추가, 게임 끝 구현

--- src/Main.java
+++ src/Main.java
@@ -6,19 +6,51 @@ import java.awt.event.KeyListener;
 import javax.sound.sampled.*;
 import javax.swing.*;
 import java.io.IOException;
-import java.util.Iterator;
-import java.util.Stack;
+import java.util.*;
 import java.util.Timer;
-import java.util.TimerTask;
-import static java.lang.Math.random;
+
 class Mole{
+    Image dig =new ImageIcon(Main.class.getResource("./image/digda.png")).getImage();
+    Image digtrio =new ImageIcon(Main.class.getResource("./image/digtrio.png")).getImage();
+    Image aloladig =new ImageIcon(Main.class.getResource("./image/aloladigda.png")).getImage();
+    Image aloladigtrio =new ImageIcon(Main.class.getResource("./image/alolatrio.png")).getImage();
     Image molek;
+    int special =0;
+    int birth;
+    boolean trio=Math.random()*5>3?true:false;
+    boolean hard;
     int x,y;
     boolean life=true;
-    public Mole(int x,int y,Image k){
+    public Mole(int x,int y,boolean k,int t){
         this.x=x;
         this.y=y;
-        this.molek=k;
+        this.hard=k;
+        this.birth=t;
+        if(hard==true){
+            if(trio==true)
+                molek=aloladigtrio;
+            else
+                molek=aloladig;
+        }
+        else if(hard==false){
+            if(trio==true)
+                molek=digtrio;
+            else
+                molek=dig;
+        }
+    }
+    public Mole(int x,int y,boolean k,int t,int a){
+        this.x=x;
+        this.y=y;
+        this.hard=k;
+        this.birth=t;
+        this.special=a;
+        if(hard==true){
+            molek=aloladig;
+        }
+        else if(hard==false){
+            molek=dig;
+        }
     }
     public void Printmole(Graphics g){
         if(life)
@@ -29,45 +61,28 @@ class Mole{
         try{
             Clip clip = AudioSystem.getClip();
             AudioInputStream inputStream = AudioSystem.getAudioInputStream(
-                    Main.class.getResourceAsStream("./sound/dead.wav"));
+                    Main.class.getResourceAsStream(special==0?"./sound/dead.wav":"./sound/start.wav"));
             clip.open(inputStream);
             clip.start();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
-    public void Catchmole(int mx, int my){
-        Rectangle ham = new Rectangle(mx, my, 135, 135);
-        Rectangle mol = new Rectangle(x, y, 50,50);
-        if(mol.intersects(ham))
-            Killmole();
-    }
 }
 public class Main extends JFrame implements KeyListener {
-    Stack<Mole> mole = new Stack<>();
+    Queue<Mole> mole = new LinkedList<>();
     Iterator<Mole> itr = mole.iterator();
     int x=100, y=100;
-    int count=0;
+    boolean hardmode=false;
+    int gamemode=0;//0 시작전,1게임중,2게임끝
+    int count=60;
     static int score=0;
     public final int Width =1024;
     public final int Height =666;
     Image hammer = new ImageIcon(Main.class.getResource("./image/hammer.png")).getImage();
-    Image dig =new ImageIcon(Main.class.getResource("./image/digda.png")).getImage();
-    Image bg;//= new ImageIcon(Main.class.getResource(".image/grass.png")).getImage();
+    Image bg= new ImageIcon(Main.class.getResource("./image/grass.png")).getImage();
 
     public Main(){
-        Timer timer=new Timer();
-        TimerTask task = new TimerTask() {
-            @Override
-            public void run() {
-                count++;
-                if(count%3==0){
-                    mole.add(new Mole((int)(Math.random()*900)+50,(int)(Math.random()*450)+100,dig));
-                }
-                repaint();
-            }
-        };
-        timer.schedule(task,1000,1000);
         addKeyListener(this);
         setTitle("mole game");
         setSize(Width,Height);
@@ -76,6 +91,41 @@ public class Main extends JFrame implements KeyListener {
         setBounds(0,0,1024,666);
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setGame();
+    }
+    public void setGame(){
+        switch(gamemode){
+            case 0:
+                mole.add(new Mole(300, 300, false,100,1));
+                mole.add(new Mole(700, 300, true,100,2));
+                break;
+            case 1:
+                System.out.println("gamestart");
+                Timer timer = new Timer();
+                TimerTask task = new TimerTask() {
+                    @Override
+                    public void run() {
+                        count--;
+                        if(count==0){
+                            gamemode=2;
+                        }
+                        if (count % 2 == 0) {
+                            System.out.println("spawn");
+                            for(int i=0;i<Math.random()*4;i++)
+                                mole.add(new Mole((int) (Math.random() * 800) + 50, (int) (Math.random() * 450) + 100, hardmode,count));
+                        }
+                        while(mole.size()>0&&mole.peek().birth>count+(hardmode==true?1:2)){
+                                System.out.println("despawn");
+                                mole.remove();
+                        }
+                        repaint();
+                    }
+                };
+                timer.schedule(task, 1000, 1000);
+                break;
+            case 2:
+                break;
+        }
     }
     @Override
     public void keyPressed(KeyEvent e){
@@ -115,11 +165,22 @@ public class Main extends JFrame implements KeyListener {
                         public void run() {
                             hammer = new ImageIcon(Main.class.getResource("./image/hammer.png")).getImage();
                             for(Mole i:mole){
-                                Rectangle ham = new Rectangle(x, y, 135, 135);
-                                Rectangle mol = new Rectangle(i.x, i.y, 50,50);
-                                if(mol.intersects(ham)){
+                                Rectangle ham = new Rectangle(x, y, 30, 30);
+                                Rectangle mol = new Rectangle(i.x, i.y, i.trio?70:50,i.trio?70:50);
+                                if(mol.intersects(ham)&&i.life==true){
                                     i.Killmole();
-                                    score+=10;
+                                    if(i.special>0) {
+                                        gamemode = 1;
+                                        if(i.special==2)
+                                            hardmode=true;
+                                        mole.clear();
+                                        setGame();
+                                        repaint();
+                                        break;
+                                    }
+                                    else {
+                                        score += i.trio?30:10;
+                                    }
                                 }
                             }
                             repaint();
@@ -136,18 +197,40 @@ public class Main extends JFrame implements KeyListener {
         g.clearRect(0,0,Width,Height);
         g.drawImage(bg, 0,0,null);
         //g.drawRect(50,100,950,500);
-        System.out.println(count);
-        g.setColor(Color.RED);
-        g.setFont(new Font("Serif",Font.PLAIN,40));
-        g.drawString("시간 : "+count,470,70);
-        g.drawString("점수 : "+score,800,70);
-        for(Mole i:mole){
-            i.Printmole(g);
+        switch(gamemode){
+            case 0:
+                for (Mole i : mole) {
+                    i.Printmole(g);
+                }
+                g.setColor(Color.gray);
+                g.fillRect(295,355,60,20);
+                g.fillRect(695,355,60,20);
+                g.setColor(Color.white);
+                g.drawString("이지모드", 300, 370);
+                g.drawString("하드모드", 700, 370);
+                break;
+            case 1:
+                System.out.println(count);
+                g.setColor(Color.RED);
+                g.setFont(new Font("Serif", Font.PLAIN, 40));
+                g.drawString("시간 : " + count, 470, 70);
+                g.drawString("점수 : " + score, 800, 70);
+                for (Mole i : mole) {
+                    i.Printmole(g);
+                }
+                break;
+            case 2:
+                g.setColor(Color.RED);
+                g.setFont(new Font("Serif", Font.PLAIN, 100));
+                g.drawString("끝났습니다!", 400, 200);
+                g.setFont(new Font("Serif", Font.PLAIN, 50));
+                g.drawString("점수 : " + score, 400, 400);
+                break;
         }
         g.setColor(Color.blue);
-        g.drawOval(x,y,30,30);
-        g.drawOval(x-5,y-5,40,40);
-        g.drawImage(hammer,x,y,null);
+        g.drawOval(x, y, 30, 30);
+        g.drawOval(x - 5, y - 5, 40, 40);
+        g.drawImage(hammer, x, y, null);
     }
     @Override
     public void keyReleased(KeyEvent e) {
